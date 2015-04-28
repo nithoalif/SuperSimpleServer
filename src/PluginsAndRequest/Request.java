c8443aa3bb65b487d934199d6022a327d9590138
SatriaPriambada <satriapriambada@gmail.com>
2015-04-29 02:17:23 +0700

    gunakan plugin prerequest pada kelas request

--- src/PluginsAndRequest/Request.java
+++ src/PluginsAndRequest/Request.java
@@ -62,6 +62,17 @@ public class Request {
         state = enumState.processed;
         Map result = new HashMap();
         PluginLoader plugins = PluginLoader.getInstance();
+        //Menjalankan prerequest plugin
+        for(Object o : plugins.GetProcessRequestList()){
+            try {
+                System.out.println(o.getClass().getName());
+                Method m = o.getClass().getDeclaredMethod("preprocess", Object.class, Map.class); 
+                m.invoke(o, (Object)this, result);
+            } catch (Exception ex) {
+                Logger.getLogger(Request.class.getName()).log(Level.SEVERE, null, ex);
+            }    
+        }
+        //Menjalankan process request plugin
         for(Object o : plugins.GetProcessRequestList()){
             try {
                 System.out.println(o.getClass().getName());
@@ -71,6 +82,7 @@ public class Request {
                 Logger.getLogger(Request.class.getName()).log(Level.SEVERE, null, ex);
             }    
         }
+        //menjalankan post request plugin
         for(Object o : plugins.GetPostRequestList()){
             try {
                 System.out.println(o.getClass().getName());
